84abb880442a33c54adeb919d69ac349b4ce73c8
saudi <saud.vectorcoder@gmail.com>
2020-06-16 11:40:12 +0500

    Fabric to Firebase cnversion v1.7

--- app/src/main/java/com/sunztech/abudawood/MyApplication.java
+++ app/src/main/java/com/sunztech/abudawood/MyApplication.java
@@ -5,10 +5,8 @@ import android.app.Application;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
-import com.crashlytics.android.Crashlytics;
 import com.onesignal.OneSignal;
 
-import io.fabric.sdk.android.Fabric;
 
 public class MyApplication extends Application {
 
@@ -20,7 +18,6 @@ public class MyApplication extends Application {
 
 
         super.onCreate();
-        Fabric.with(this, new Crashlytics());
         // OneSignal Initialization
         OneSignal.startInit(this)
                 .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
